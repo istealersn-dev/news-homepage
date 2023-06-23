6d5221edf447c4121185cdce75efffc307a9a270
istealersn.dev <istealersn.dev@gmail.com>
2023-06-23 23:58:11 +0530

    Initialised the base setup
    Have completed the folder structure, Vite config and base setup

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import "@fontsource/inter/400.css"
+import "@fontsource/inter/700.css"
+import "@fontsource/inter/800.css"
+import './styles/styles.scss'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    Welcome to my project
+  </div>
+`
