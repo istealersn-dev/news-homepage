0650d8869f898b6e7d35ec0a214727b5496d9092
istealersn.dev <istealersn.dev@gmail.com>
2023-07-02 16:27:50 +0530

    fixed issues and completed the build

--- src/components/Hamburger.ts
+++ src/components/Hamburger.ts
@@ -2,38 +2,48 @@ import { HeaderData } from "../types";
 
 const Hamburger = ({menuClose, menuItems }: HeaderData) => {
 
-    const hamburger = document.createElement('div');
-    hamburger.classList.add('hamburger');
-    hamburger.setAttribute('aria-disabled', 'true')
+    const viewport = window.innerWidth
 
-    const hamburgerMenu = document.createElement('div');
-    hamburgerMenu.classList.add('hamburger__menu');
+    if (viewport < 768) {
 
-    const close = document.createElement('img');
-    close.classList.add('hamburger__menu--close')
+        const hamburger = document.createElement('div');
+        hamburger.classList.add('hamburger');
+        hamburger.setAttribute('aria-disabled', 'true')
 
-    const ulItems = document.createElement('ul');
-    ulItems.classList.add('hamburger__menu--items');
-    
-        close.src = menuClose ? menuClose : ''
-        hamburgerMenu.appendChild(close)
+        const hamburgerMenu = document.createElement('div');
+        hamburgerMenu.classList.add('hamburger__menu');
 
-        if (menuItems) {
-            menuItems.forEach( menuItem => {
-            const listItem = document.createElement('li')
-            const listLink = document.createElement('a')
+        const close = document.createElement('img');
+        close.classList.add('hamburger__menu--close')
 
-            listLink.href = menuItem.link
-            listLink.textContent = menuItem.item
+        const ulItems = document.createElement('ul');
+        ulItems.classList.add('hamburger__menu--items');
+        
+            close.src = menuClose ? menuClose : ''
+            hamburgerMenu.appendChild(close)
 
-            listItem.appendChild(listLink)
-            ulItems.appendChild(listItem)
-        })} else ''
+            if (menuItems) {
+                menuItems.forEach( menuItem => {
+                const listItem = document.createElement('li')
+                const listLink = document.createElement('a')
 
-    hamburgerMenu.appendChild(ulItems)
-    hamburger.appendChild(hamburgerMenu)
+                listLink.href = menuItem.link
+                listLink.textContent = menuItem.item
 
-    return hamburger
+                listItem.appendChild(listLink)
+                ulItems.appendChild(listItem)
+            })} else ''
+
+        hamburgerMenu.appendChild(ulItems)
+        hamburger.appendChild(hamburgerMenu)
+
+        return hamburger
+    } else {
+        const hamburgerParent = document.querySelector(".header__nav")
+        const hamburger = document.querySelector(".hamburger") 
+        if (hamburger) hamburgerParent?.removeChild(hamburger)
+        return document.createElement("div")
+    }
 }
 
 export default Hamburger
--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,8 +1,9 @@
 // import Hamburger from "./Hamburger";
 
 import { HeaderData } from "../types";
+import { Div } from "./atoms/Div";
 
-const Header = ({logo, menuIcon}: HeaderData)  => {
+const Header = ({logo, menuIcon, menuClose, menuItems}: HeaderData)  => {
     const header = document.createElement("header");
     header.classList.add("header");
 
@@ -12,22 +13,76 @@ const Header = ({logo, menuIcon}: HeaderData)  => {
     const navLogo = document.createElement("img");
     navLogo.classList.add("header__nav--logo");
 
-    const mobileMenu = document.createElement("div");
-    mobileMenu.classList.add("header__nav--mobile");
-    mobileMenu.setAttribute('aria-controls', "hamburger-menu")
-    mobileMenu.setAttribute('aria-label', "Toggle menu")
+    let desktopMenuRendered = false;
+    let moblieMenuRendered = false;
 
-    const menuBar = document.createElement("img");
-    menuBar.classList.add("header__nav--mobile-menu");
+    const renderMenu = () => {
 
-    mobileMenu.appendChild(menuBar);
+        const viewport = window.innerWidth
+
+        if (viewport > 768) {
+
+            // remove existing mobile menu if it exists
+            const mobileMenu = navItems.querySelector(".header__nav--mobile")
+            if (mobileMenu) navItems.removeChild(mobileMenu)
+
+            if (!desktopMenuRendered) {
+            const desktopMenu = Div();
+            const navList = document.createElement('ul')
+        
+            if (menuItems) {
+                menuItems.forEach(menuItem => {
+                    const listItem = document.createElement('li')
+                    const listLink = document.createElement('a')
+        
+                    listLink.href = menuItem.link
+                    listItem.textContent = menuItem.item
+        
+                    listItem.appendChild(listLink)
+                    navList.appendChild(listItem)
+                })
+            }
+        
+            desktopMenu.appendChild(navList)
+            desktopMenu.classList.add('header__nav--desktop')
+            navItems.appendChild(desktopMenu)
+        }
+
+            desktopMenuRendered = true
+            moblieMenuRendered = false
+
+        } else {
+
+            // remove existing desktop menu if it exists
+            const desktopMenu = navItems.querySelector(".header__nav--desktop")
+            if (desktopMenu) navItems.removeChild(desktopMenu)
+
+            if (!moblieMenuRendered) {
+                const mobileMenu = document.createElement("div");
+                mobileMenu.classList.add("header__nav--mobile");
+                mobileMenu.setAttribute('aria-controls', "hamburger-menu")
+                mobileMenu.setAttribute('aria-label', "Toggle menu")
+            
+                const menuBar = document.createElement("img");
+                menuBar.classList.add("header__nav--mobile-menu");
+                menuBar.src = menuIcon
+        
+            mobileMenu.appendChild(menuBar);
+            navItems.appendChild(mobileMenu);
+            }
+
+            desktopMenuRendered = false
+            moblieMenuRendered = true
+        }
+    }
 
     navLogo.src = logo
-    menuBar.src = menuIcon
-
     navItems.appendChild(navLogo);
-    navItems.appendChild(mobileMenu);
     header.appendChild(navItems);
+
+    // Update image function callback for load and while resizing window
+    window.addEventListener('load', renderMenu)
+    window.addEventListener('resize', renderMenu)
 
     return header;
 };
